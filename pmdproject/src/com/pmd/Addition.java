ee836a669dca1a1d12eb2c81f8f7a3b50afa34b0
Pallavi <Pallavi@DESKTOP-GLNSG25>
2019-07-13 17:54:42 +0530

    pmdproject is done pmd is use to anaysis source code

---
+++ pmdproject/src/com/pmd/Addition.java
@@ -0,0 +1,11 @@
+package com.pmd;
+public class Addition {
+	
+static	int a=10;
+	static int b;
+   int d;
+	public static void main(String[] args) {
+		System.out.println(a);
+		System.out.println(b);
+		
+	}	}
